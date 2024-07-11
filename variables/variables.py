f7efc6177e71d4ad4e27e195282d7e1c40e99c24
richard carvajal <richardfernelcarvajalvillamiza@gmail.com>
2024-07-10 20:47:49 -0500

    avanzamos

---
+++ variables/variables.py
@@ -0,0 +1,25 @@
+a=5
+b=8
+c=a+b
+print(c)
+
+nombre="richard"
+print("hola " +  nombre)
+
+numero=10
+print(numero)
+numero=10+1
+print(numero)
+numero+=1
+print(numero)
+numero+=3
+print(numero)
+numero=10
+numero+=5
+numero-=5
+print(numero)
+nombre1="richard"
+bienvenida="hola "+ nombre1 + " feliz noche"
+print(bienvenida)
+
+
