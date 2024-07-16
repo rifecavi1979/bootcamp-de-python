df41a74c8cb4b78b82ff88d1fbe68fe565f02c5f
richard carvajal <richardfernelcarvajalvillamiza@gmail.com>
2024-07-15 19:47:53 -0500

    fundamentos de python

---
+++ fundamentos.py
@@ -0,0 +1,17 @@
+# como saber el tipo de datos de una variable
+name = "Richard"
+print(type(name))
+print(name[0])
+print(name[1:3])
+last_name = "carvajal villamizar"
+print(name+" "+last_name)
+print(name*5)
+print(len(name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
+
+
+
+
+
